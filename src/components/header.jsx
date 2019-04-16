97f2e80b322731ee485fc23b70a0a592bf4d1b8f
Administrator <administrator@CN-4.local>
2019-04-16 16:51:00 +0100

    TechBar component completed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,9 +5,9 @@ const Header = props => {
   return (
     <>
       <div className="header">
-        <HeaderLabel title="STATS" active={false} />
+        <HeaderLabel title="STATS" active={true} />
         <HeaderLabel title="WORK" active={false} />
-        <HeaderLabel title="CONTACT" active={true} />
+        <HeaderLabel title="CONTACT" active={false} />
       </div>
       <div className="header-spacer" />
     </>
--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./main-content.css";
 import vaultboy from "../resources/vaultboy.gif";
+import TechBar from "./techBar";
 
 const Stats = props => {
   return (
     <div className="main-content">
+      <TechBar height="15px" width="90px" label="Javascript" level="75" />
+
       <img id="vault-boy" src={vaultboy} alt="Vault Boy animation" />
+      <TechBar height="15px" width="90px" label="CSS" level="75" />
     </div>
   );
 };
