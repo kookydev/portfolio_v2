3b72e47096e44b4dd9d78ea9f6c5eb45c2737a28
Administrator <administrator@CN-4.local>
2019-04-16 17:11:55 +0100

    couple of tweaks

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -6,10 +6,16 @@ import TechBar from "./techBar";
 const Stats = props => {
   return (
     <div className="main-content">
-      <TechBar height="15px" width="90px" label="Javascript" level="75" />
+      <TechBar
+        id="tech1"
+        height="15px"
+        width="90px"
+        label="Javascript"
+        level="95"
+      />
 
       <img id="vault-boy" src={vaultboy} alt="Vault Boy animation" />
-      <TechBar height="15px" width="90px" label="CSS" level="75" />
+      <TechBar id="tech2" height="15px" width="90px" label="CSS" level="80" />
     </div>
   );
 };
