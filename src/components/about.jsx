bd4d8e23fc24366c4991c82de648930e75239315
Kyle <kwjtdev@gmail.com>
2019-04-29 17:10:17 +0100

    Coplete

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -20,17 +20,21 @@ const About = props => {
           className="avatar"
         />
         <div className="modal-text">
-          Name: Kyle <br />
-          Likes: Writing Code, Playing guitar, Singing, Learning new things,
-          Pushing my limits, The occasional cookie... <br />
-          Loves: My family <br />
-          Dislikes: Negativity (is that ironic?), Admitting defeat... <br />
-          About: I'm a Junior Full-stack Developer based in the Chester (UK)
-          area. After years in a career which I was good at, but in which I
-          never felt I belonged, I took the plunge into this wondrous coding
-          adventure! I love creating awesome things with tech. Check out some of
-          my work below and, if you like what you see, or want to hire me 😉,
-          let me know.
+          <p>Hi, I'm Kyle!</p>
+          <p>
+            I'm a Junior Full-stack Developer based in the Chester (UK) area.
+            After years in a career which I was good at, but in which I never
+            felt I belonged, I took the plunge into this wondrous coding
+            adventure! I love creating awesome things with tech. Check out some
+            of my work below and, if you like what you see, or want to hire me
+            😉, let me know.
+          </p>
+          <p>
+            Likes: Writing Code, Playing guitar, Singing, Learning new things,
+            Pushing my limits, The occasional cookie...
+          </p>
+          <p>Loves: My family</p>
+          <p>Dislikes: Negativity (is that ironic?), Admitting defeat...</p>
         </div>
       </div>
     </div>
