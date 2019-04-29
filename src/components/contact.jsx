67a974ea09fa81448ffb8f3bf94b72a000685467
Kyle <kwjtdev@gmail.com>
2019-04-29 16:25:51 +0100

    contact complete

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,22 +1,27 @@
 import React from "react";
+import "./contact.css";
 
 const Contact = props => {
   return (
     <div className="contact-container">
-      <div className="social-container">
-        <a href="">
-          <i class="fab fa-twitter-square" />
+      <h2>Follow me:</h2>
+      <div className="contact-container-child">
+        <a href="https://github.com/kookydev">
+          <i class="fab fa-github-square" /> github.com/kookydev
         </a>
-        <a href="">
-          <i class="fab fa-linkedin" />
+        <a href="https://linkedin.com/in/kyle-trotter">
+          <i class="fab fa-linkedin" /> linkedin.com/in/kyle-trotter
         </a>
-        <a href="">
-          <i class="fas fa-envelope-square" />
+      </div>
+      <h2>Contact me:</h2>
+      <div className="contact-container-child">
+        <a href="mailto:info@kooky.dev">
+          <i class="fas fa-envelope-square" /> info@kooky.dev
         </a>
-        <a href="">
-          <i class="fas fa-phone-square" />
+        <a href="tel:+447726152415">
+          <i class="fas fa-phone-square" /> 07726 152415
         </a>
-      </div>{" "}
+      </div>
     </div>
   );
 };
